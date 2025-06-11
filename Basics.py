1a0e61ba1709200e6343a8765c294f3ab9581ceb
KasoziJoel <joelmet256@gmail.com>
2025-06-11 01:40:29 -0700

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,20 @@
+# <- single line comment
+"""
+Multi line comment
+
+this is another commented line
+"""
+
+# print("this is my name") 
+
+# Python coding Case (Snake case)
+"""
+first_name
+joker_boy
+"""
+
+# Indent
+first_name = "jj"
+print(first_name)
+
+ 
