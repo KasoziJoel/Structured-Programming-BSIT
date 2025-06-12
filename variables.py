6072226eba28f32444a1ffcd72fa5b74bdf36edc
KasoziJoel <joelmet256@gmail.com>
2025-06-11 21:44:44 -0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+## Assigning
+## Declaring
+
+## value
+
+## data type
+"""
+integers
+floats
+strings/characters
+Boolean
+"""
+
+number_one = 30 # int
+number_two = 3.17 #float 
+last_name = 'john'
+first_name = "joker"
+is_attractive = False
+is_present = True 
+
+## checking for datatype
+print(type(number_two)) 
+
+## conversions
+new_number_two = int(number_two)
+print(f"integer: {new_number_two} datatype: {type(number_two)}")
+
+
+  
