027ce9294bc32bd7b15a30c76fd2274eccdeab67
KasoziJoel <joelmet256@gmail.com>
2025-06-19 17:56:09 +0300

    Add files via upload

---
+++ j[1].py
@@ -0,0 +1,45 @@
+
+
+user_name = "John Doe" 
+print(f"Good morning, {user_name}")
+
+user_name = "Jane Doe"
+print(f"GM,{user_name}") 
+
+sum = 1+1
+print(f"one plus one is equal to  {sum}👍")
+
+def function_name ():
+    return"anything you want!"
+print(function_name())
+
+
+
+def greet_user(x):
+    print(f"evening, {x}😈")
+
+greet_user("Jowl")
+greet_user("kkk")
+greet_user("jjj")
+
+
+def sum_of_numbers(a,b):
+    return(a+b)
+print(sum_of_numbers(6,7))
+
+
+def square_of_number(x):
+    return(x*x)
+print(square_of_number(6))
+
+
+
+
+print("smthng", end="\t")
+print("smthng2") 
+
+print("smthng", end="\n")
+print("smthng2")
+
+
+
