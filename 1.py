5f48178c12b91a02cef68e49eff479df60395ad6
KasoziJoel <joelmet256@gmail.com>
2025-06-22 05:34:55 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+def main():
+    print("Welcome to indentation race!")
+
+for i in range(5):
+    print("current nummber:", i)
+        if i % 2 == 0:
+            print("Even number.")
+        else:
+            print("Odd number.")
+    print("Race completed!")
+main():
+    
+
---
+++ 5.py
@@ -0,0 +1,10 @@
+def display_student_info(name_age):
+    print("Student Name:", Name)
+    print("student Age:", age)
+    if age < 18:
+        print("status:Minor")
+    else:
+        print("Status Adult")
+    
+    student_name = "Alice"
+student_age = 20
