7106bb697bd8f6e45a15d73f1d1e9672a79dabd8
KasoziJoel <joelmet256@gmail.com>
2025-06-27 12:00:01 +0300

    Add files via upload

--- while.py
+++ while.py
@@ -1,18 +1,5 @@
-age =15
-if age ==18:
-    print("Vote")
-else:
-    print("No")
-    
-AGE=int(input("enter age"))
-if AGE >=18:
-    print("Drink")
-else:
-    print("Eat")
-    
-num =13
-if num %2 ==0:
-    print("even")
-else:
-    print("odd")
-    
+def sum(a,b):
+    return(a+b)
+
+print(sum(1,9))
+
