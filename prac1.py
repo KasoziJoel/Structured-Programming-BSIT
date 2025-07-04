b3bfafaf51f23fc1c3e19a301d5ae7ebd2f11550
KasoziJoel <joelmet256@gmail.com>
2025-07-04 22:27:11 +0300

    Add files via upload

---
+++ prac1.py
@@ -0,0 +1,16 @@
+
+
+length = float(input("Enter the length"))
+
+width = float(input("Enter the width"))
+
+#calculate area
+area = length*width
+print(f"Area: {area}")
+
+#calculate perimeter
+perimeter = 2* (length+width)
+print(f"Perimeter: {perimeter}")
+
+
+
