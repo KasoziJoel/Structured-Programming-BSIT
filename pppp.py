60ec8eba7f0998f2933c9c47f6109689b1a33ae5
KasoziJoel <joelmet256@gmail.com>
2025-07-06 14:23:33 +0300

    Add files via upload

---
+++ pppp.py
@@ -0,0 +1,42 @@
+
+
+def add_numbers(n1,n2):
+    result = n1 + n2
+    return result
+
+
+n1 = 3.9
+n2 = 50
+result = add_numbers(n1,n2)
+
+print("The sum is", result)
+
+
+
+
+def greet(x):
+    print("Hello", x, end="\t")
+    print("How r u doing?")
+
+greet("JOEl")
+greet("Kasozi")
+
+
+
+
+print("smthng", end="\t")
+print("tttt")
+print("tttt")
+
+
+
+marks = (55, 64, 75, 80, 65)
+length = len(marks)
+
+print("The length is", length)
+
+sum = sum(marks)
+print("The total marks you got are", sum)
+
+
+
