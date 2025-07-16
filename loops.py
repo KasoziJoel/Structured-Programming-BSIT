fc0de318376b82559d7258b5862e3bcf2c3a5830
KasoziJoel <joelmet256@gmail.com>
2025-07-16 13:32:53 +0300

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,22 @@
+#for
+
+for i in range(1, 201):
+    if i % 2 == 0:
+        print(i)
+
+for i in range(1, 3):
+    for y in range(1, 3):
+        print(f"{i},{y}")
+
+#while
+total = 0
+while total < 101:
+    print(total)
+    total+=1
+
+
+
+
+
+
+
